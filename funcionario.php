affdd22ce45016c306eb5b3a744be4955bbe0a04
victor-pereira0411 <victorgabrielpereira823@gmail.com>
2024-05-15 11:10:37 -0300

    modal atualizar feito, alert deletado feito

--- funcionario.php
+++ funcionario.php
@@ -115,10 +115,6 @@
                                     <div class="modal-body">
                                         <form class="needs-validation" action="cruds/cadFuncionario.php" method="post">
                                             <div class="mb-3">
-                                                <label for="text" class="form-label">Matrícula</label>
-                                                <input type="text" class="form-control" id="matrifunc" name="matrifunc">
-                                            </div>
-                                            <div class="mb-3">
                                                 <label for="text" class="form-label">Nome do funcionário</label>
                                                 <input type="text" class="form-control" id="nomfunc" name="nomfunc" required>
                                             </div>
@@ -136,22 +132,49 @@
                             </div>
                         </div>
                     </div>
-
-                    <div>
-                        <?php
-                        if (isset($_GET['sucesso'])) {
-                            $nomefun = $_GET['nome_funcionario'];
-                        ?>
-                            <div class="container">
-                                <div class='alert alert-success alert-dismissible fade show' role='alert'>
-                                    <?php echo $nomefun ?> cadastrado com sucesso!
-                                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                    <div class="modal fade" id="modaleditar" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Editar funcionário</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form class="needs-validation" action="cruds/cadFuncionario.php" method="post">
+                                        <div class="mb-3">
+                                            <label for="text" class="form-label">Nome do funcionário</label>
+                                            <input type="text" class="form-control" id="nomfunc" name="nomfunc" required>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="int" class="form-label">Ganho por milheiro</label>
+                                            <input type="int" class="form-control" id="ganho" name="ganhoFunc" required>
+                                        </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                                    <button type="submit" class="btn btn-primary" name="btnfunc">Cadastrar</button>
+                                    </form>
                                 </div>
                             </div>
-                        <?php
-                        } else if (isset($_GET['delete'])) {
-                            $nomefundel= $_GET['nome_funcionario'];
-                        ?>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <?php
+                    if (isset($_GET['sucesso'])) {
+                        $nomefun = $_GET['nome_funcionario'];
+                    ?>
+                        <div class="container">
+                            <div class='alert alert-success alert-dismissible fade show' role='alert'>
+                                <?php echo $nomefun ?> cadastrado com sucesso!
+                                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                            </div>
+                        </div>
+                    <?php
+                    } else if (isset($_GET['delete'])) {
+                        $nomefundel = $_GET['nome_funcionario'];
+                    ?>
 
                         <div class="container">
                             <div class='alert alert-danger alert-dismissible fade show' role='alert'>
@@ -160,54 +183,54 @@
                             </div>
                         </div>
 
-                        <?php
-                        }
-                        ?>
+                    <?php
+                    }
+                    ?>
 
-                    </div>
                 </div>
-                <?php
-                if (count($funcionarios) > 0) {
-                ?>
-                    <div class="table-responsive m-4 d-flex justify-content-center align-items-center">
-                        <table class="table">
-                            <thead>
-                                <th>matrícula</th>
-                                <th>nome</th>
-                                <th>Ganho(milheiro)</th>
-                                <th>Ações</th>
-                            </thead>
-                            <tbody>
-                                <?php
-                                foreach ($funcionarios as $funcionario) {
-                                    echo "<tr>";
-                                    echo "<td>" . $funcionario['matricula'] . "</td>";
-                                    echo "<td>" . $funcionario['nome'] . "</td>";
-                                    echo "<td>" . $funcionario['ganhoMilheiro'] . "</td>";
-                                    echo "<td>" . "<div class='d-flex flex-row'><button class='btn btn-warning'>editar</button>";
-                                    echo "
+            </div>
+            <?php
+            if (count($funcionarios) > 0) {
+            ?>
+                <div class="table-responsive m-4 d-flex justify-content-center align-items-center">
+                    <table class="table">
+                        <thead>
+                            <th>matrícula</th>
+                            <th>nome</th>
+                            <th>Ganho(milheiro)</th>
+                            <th>Ações</th>
+                        </thead>
+                        <tbody>
+                            <?php
+                            foreach ($funcionarios as $funcionario) {
+                                echo "<tr>";
+                                echo "<td>" . $funcionario['matricula'] . "</td>";
+                                echo "<td>" . $funcionario['nome'] . "</td>";
+                                echo "<td>" . $funcionario['ganhoMilheiro'] . "</td>";
+                                echo "<td>" . "<div class='d-flex flex-row'><button type='button' class='btn btn-warning' data-bs-toggle='modal' data-bs-target='#modaleditar'>editar</button>";
+                                echo "
                                     <form action='cruds/delFuncionario.php' method='get'> " .
-                                        "<input type='hidden' name='matricula' value='" . $funcionario['matricula'] . "'>" .
-                                        "<input type='hidden' name='nome' value='" . $funcionario['nome'] . "'>" .
-                                        "<button type='submit' class='btn btn-danger'>excluir</button>" .
-                                        "</form>
+                                    "<input type='hidden' name='matricula' value='" . $funcionario['matricula'] . "'>" .
+                                    "<input type='hidden' name='nome' value='" . $funcionario['nome'] . "'>" .
+                                    "<button type='submit' class='btn btn-danger'>excluir</button>" .
+                                    "</form>
                                     </td>";
-                                    echo "</tr>";
-                                }
-                                ?>
+                                echo "</tr>";
+                            }
+                            ?>
 
-                            </tbody>
-                        </table>
-                    </div>
-                <?php } else {
-                    echo "<div class='d-flex justify-content-center mt-5'>
+                        </tbody>
+                    </table>
+                </div>
+            <?php } else {
+                echo "<div class='d-flex justify-content-center mt-5'>
                     <h4 class=''>Você não possui funcionários cadastrados</h4>
                     </div>
                     ";
-                }
-                ?>
-            </div>
+            }
+            ?>
         </div>
+    </div>
     </div>
     <script>
         $(".sidebar ul li").on("click", function() {
